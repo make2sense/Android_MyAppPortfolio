d913e8662b065ab7b89ae748bd7999c365ba1f5e
make2sense <limengpan@gmail.com>
2015-11-05 18:59:40 -0600

    initial commit

---
+++ app/src/main/java/com/example/android/lmcloud/myappporfolio/MyActivity.java
@@ -0,0 +1,93 @@
+package com.example.android.lmcloud.myappporfolio;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        // Below is my codes for the buttons activities
+        // button = (Button) findViewById(R.id.buttonAlert);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void project1_clicked(View v) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch Popular Movies my app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+    public void project2_clicked(View v) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my Scores app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+    public void project3_clicked(View v) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my Library app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+    public void project4_clicked(View v) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my Build It Bigger app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+    public void project5_clicked(View v) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my XYZ Reader app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+    public void project6_clicked(View v) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my Capstone app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+}
